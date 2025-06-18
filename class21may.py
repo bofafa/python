345381597a04c10b5cca1f9a14cb638290c9c9bf
bofafa <bopuinar@gmail.com>
2025-06-18 21:56:55 +0800

    add new files

--- class21may.py
+++ class21may.py
@@ -226,3 +226,224 @@ car = {
         'build' : a() #build 裝埋funtion a's result,
     }
 print(car['build'])  #I am for funtion a , none
+
+print("----------- 12 June -----------------")
+x = 10
+if x <= 9:
+    print("x is less than and equal to 9")
+elif x> 9 and x == 10:
+    print("x is equal to 10")
+else :print ("x is larger than 10")
+
+print("----------- truthy value -----------------")
+x = 1 and 0 and True and [1] and {1} and {"1" :1} and 1.1 and (1,)
+print (x)
+
+
+print("----------- default value -----------------")
+default = "TST"
+city = ''
+city = city or print ("please inout city") or default
+print (city)
+
+
+print("----------- while loop -----------------")
+x = 10
+while (x):
+    x= x -1 # <---- controller
+    if x  == 7:
+        continue
+    print(x)
+    if x == 4:
+        break 
+else:
+    print ("exit when while is false")
+
+
+print("-----------for loop-----------------")
+for i in range(3):       #(0,3,1)
+    print(i) #range(start, end - 1, step)
+
+print("-----------range (start : end - 1: step)----------------")
+for i in range(1,5,2): 
+    print(i) #range(start, end - 1, step)
+
+print("----------- Range 2-----------------")
+for i in [1,2,3,4]:
+    print(i)
+
+
+
+print("----------- while loop , break-----------------")
+def a (x):
+    return x
+b = 1
+while (a(b)):
+    print(b)
+    b = b + 1
+    if b == 10:     #if
+        break       #break
+
+
+print("----------- slice ----------------")
+l = [1,2,3,4,5,6,7,8,9,10,11,12]
+x = l[0:4:1]
+start,end,step = 0,9,3
+s = slice (start,end,step)
+x = l[s]
+print (x)
+
+
+print("----------- backward----------------")
+#      0,   1,   2,  3,  4,  5,  6,  7,  8,  9, 10, 11
+l = [  1,   2,   3,  4,  5,  6,  7,  8,  9, 10, 11, 12]
+#    -12, -11, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1
+x = l[-4 : -9 : -1]
+print(x)
+
+print("----------- del list item----------------")
+l = [  1,   2,   3,  4,  5,  6,  7,  8,  9, 10, 11, 12]
+del l[3:6]
+print (l)
+
+
+print("----------- 18 June : try , except---------------")
+try:
+    x = 1
+    y = "0"
+    z = x/y
+    print(z)
+except ZeroDivisionError:
+    print(" Division error, please fix Y value")
+except TypeError:
+    print("please check data type, values must be number type")
+else:
+    print("no error")
+finally:   #file closed
+    print("this line will run, not matter what")
+
+
+print("-----------range (start : end - 1: step)----------------")
+
+
+print("#1")
+lst = [0,1,2,3,4,5]
+print (lst[1:4]) #1,2,3
+print("#2")
+lst = [0,1,2,3,4,5]
+print (lst[:3]) #0,1,2
+print("#3")
+lst = ["a","b","c","d"]
+print (lst[2:]) #c,d
+print("#4")
+lst = [10,20,30,40,50]
+print (lst[:-2]) #3號位停  10,20,30
+print("#5")
+lst = ["apple", "banana", "cherry", "date"]
+print(lst[-2:]) #cherry, date
+print("#6")
+lst = [0,1,2,3,4,5]
+print(lst [::2]) #0,2,4
+print("#7")
+lst = ["p","y","t","h","o","n"]
+print(lst [::-1]) #n, o, h, t, y, p
+print("#8")
+lst = [0,1,2,3,4,5]
+print (lst[4:1:-1]) #4,3,2
+print("#9")
+lst = [0,1,2,3,4,5]
+print (lst[-2:-5:-1]) #4,3,2
+print("#10")
+lst = [10,20,30,40]
+print (lst[::-2]) #40,20
+print("#11")
+lst = [0,1,2]
+print (lst[5:]) #[]
+print("#12")
+lst = ["a","b","c","d"]
+print (lst[3:1]) #[]
+print("#13")
+lst = [0,1,2,3,4,5]
+print (lst[3:0:-1]) #3,2,1
+print("#14")
+s = "hello"
+print (s[::-1]) # olleh
+print("#15")
+lst = [0,1,2,3,4,5,6]
+print (lst[::3]) #0,3,6
+print("#16")
+lst = [0,1,2,3,4,]
+print (lst[-3:4]) #2,3
+
+print("1. create a list of numbers from 0 to 9 using 'rang()'")
+print(list(range(10)))
+
+
+
+print("2. create a list of  even numbers from 2 to 20 using rang()")
+print(list(range(2,21,2)))
+
+print("3. create a list of numbers from 10 to 1 in descending order using rang()")
+print(list(range(10,0,-1)))
+
+print("4. create a list of odd numbers from 1 to 20 using rang()")
+print(list(range(1,21,2)))
+
+print("5. create a list of multiples of 5 from 5 to 50 using rang()")
+print(list(range(5,51,5)))
+
+print("6. create a list of numbers from -10 to 0 in descending order using rang()")
+print(list(range(-10,0,1)))
+
+print("7. create a list of numbers from 0 -100 that are divisible 10 using rang()")
+print(list(range(0,101,10)))
+
+print("8. creat a list of number from 1 to 10, but skip 5 using 'range()' and list comprehension.")
+print([i for i in range(1, 11) if x != 5])
+ll = []
+def temp():
+    for i in range(1,11):
+        if i !=5: 
+         ll.append(i)
+
+
+print("9. creat a list of square of numbers from 1 to 10")
+print([i**2 for i in range(1,11)])
+
+
+print("10. creat a list of numbers from 100 to 10 in step of -10 using range")
+print(list(range(100,9,-10)))
+ 
+
+print("11. create 'range() to generate a list of first 10 positive integer")
+print(list(range(1,11)))
+
+print("12. generate a list of the first 10 multiples of 3 using 'range()")
+print(list(range(3,31,3)))
+
+print("13. creat a list of numbers from 50 to 60 using range")
+print(list(range(50,61,1)))
+
+print("14. generate a list of negative even number from -1 to -20 using 'range()")
+print (list(range(-2,-21,-2 )))
+
+
+print("15. use range() to creat3e a list of number from 0 to 100, stepping by 25")
+print (list(range(0,101,25 )))
+
+print("16. create a list of numbers from 5 to 15 using range()")
+print(list(range(5,16)))
+
+
+print("17. generate a list of number from 1 to 10, but only include multiples of 2 or 3 using 'range()' and list comprehension.")
+print ([ i for i in range(0,10) if i%2 ==0 or i%3==0])
+
+print("18. generate a list of number from 10 to 100, that are divisible by 10 using range.")
+print(list(range(10,101,10)))
+
+print("19. generate a list of number from 1 to 100, that are divisible by 5 but not by 3 using range().")
+print([ i for i in range(1,101) if i%5 ==0 and i%3 !=0])
+
+print("20. use range() to generate a list of the first 10 perfect squares")
+print([i**2 for i in range(1,11)])
+
