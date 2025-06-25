ddac8ed8fb8be116deb9e47e2c2092e44b68b867
bofafa <bopuinar@gmail.com>
2025-06-25 20:57:13 +0800

    Delete pytest directory

--- pytest/test.py
+++
@@ -1,8 +0,0 @@
-def a():
-  print("I am from test.py function a")
-
-def b():
-  print("I am from test.py function b")
-
-x = 10
-_y = 20
--- pytest/util/__init__.py
+++
@@ -1,6 +0,0 @@
-# when main is loading util package _init_.py will run
-#case 3
-print("__init__ is a trigger")
-
-COLOR = 'red'  
-from .test1 import a, b, x
